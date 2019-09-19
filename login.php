924a9e9f3ed2fe75a2d29f58af5858f322de61b4
teamzolander <55356990+teamzolander@users.noreply.github.com>
2019-09-19 17:09:32 +0100

    Update login.php

--- login.php
+++ login.php
@@ -11,14 +11,14 @@ $readFile = fread($myfile,filesize("namefile.txt"));
 $arr = (explode(" ",$readFile));#The explode() make the content in $readFile to an array
 
 # This output the indexed element to the server of the login details
-echo "These are the login details of the user";
+echo "These are the login details of the user<br><br>";
 print_r("$arr[1]<br/>");
 print_r("$arr[5]<br/>");
 
 #This check the content of the array with the username and passwors of the login page
 if (($loginUsername == $arr[1]) && ($loginPassword == $arr[5])){
-    echo "This is the output message  when compared with the the sign up data";
-    echo $loginUsername ." is successful<br><br>";
+    echo "This is the output message  when compared with the the sign up data<br><br>";
+    echo $loginUsername ." is successful<br><br>"; 
     echo $loginPassword ." is successful";
     
 }
