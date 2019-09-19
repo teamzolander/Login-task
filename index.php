dea9e9d7eaa24fcc5abff7e9d12caec141688324
Dave Nnamdi <51624310+davennamdi@users.noreply.github.com>
2019-09-19 12:38:44 +0100

    index.php
    landing page

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Index</title>
+    <link rel="stylesheet" href="page.css">
+    <link rel="stylesheet" href="newFont.css">
+
+</head>
+<body>
+    <div class="wrap">
+        <div class="nav">
+            <div class="logo"> 
+                <a href="page.php">Zolander</a>
+            </div>
+            <div class="menu">
+                <ul>
+                    <li><a href="page.php">Home</a></li>
+                    <li><a href="newLogin.php">Login</a></li>
+                </ul>
+            </div>
+        </div> <!--end of nav div-->
+        <div class="header">
+            <h1>Team Zolander</h1>
+            <p>We build and maintain software</p>
+            <a href="signup.php">Sign up</a>
+
+        </div>
+    </div>
+</body>
+</html>
