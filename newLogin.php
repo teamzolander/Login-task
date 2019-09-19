c635530b252c1e1af6cd5811344d08a6065f94d4
Dave Nnamdi <51624310+davennamdi@users.noreply.github.com>
2019-09-19 12:44:56 +0100

    newLogin.php
    login page

---
+++ newLogin.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sign Up</title>
+    <link rel="stylesheet" href="newLogin.css">
+    <link rel="stylesheet" href="newFont.css">
+
+</head>
+<body>
+    <div class="wrap">
+        <div class="nav">
+            <div class="logo"> 
+                <a href="index.php">Zolander</a>
+            </div>
+        </div> <!--end of nav div-->
+        
+        <div class="signup">
+            <form action="#" method="POST">
+                <h2>Login</h2>
+               
+                <input type="text" name="username" placeholder="Enter Username">
+                <input type="password" name="password" placeholder="Enter Password">
+                <input type="submit" value="Login" class="button">
+                <a class="shift" href="index.php">Logout</a>
+                <a  href="signup.php">Don't have an account? Sign up here</a>
+
+            </form>
+        </div>
+        
+    </div>
+</body>
+</html>
