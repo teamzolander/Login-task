160b09b34d939e54631044f9ef4a24887613a667
teamzolander <55356990+teamzolander@users.noreply.github.com>
2019-09-19 17:11:27 +0100

    Update login.php

--- login.php
+++ login.php
@@ -13,10 +13,11 @@ $arr = (explode(" ",$readFile));#The explode() make the content in $readFile to 
 # This output the indexed element to the server of the login details
 echo "These are the login details of the user<br><br>";
 print_r("$arr[1]<br/>");
-print_r("$arr[5]<br/>");
+print_r("$arr[5]<br/><br>");
 
 #This check the content of the array with the username and passwors of the login page
 if (($loginUsername == $arr[1]) && ($loginPassword == $arr[5])){
+    echo "============================================================================================<br>"
     echo "This is the output message  when compared with the the sign up data<br><br>";
     echo $loginUsername ." is successful<br><br>"; 
     echo $loginPassword ." is successful";
